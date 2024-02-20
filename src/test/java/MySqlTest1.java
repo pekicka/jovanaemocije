c9e1440c37e289fa020b3b732b256fa8c1e09c1d
David Koloski <10221davidkoloski@etsntesla.edu.rs>
2024-02-20 13:35:36 +0100

    do cetvrtog videa

---
+++ src/test/java/MySqlTest1.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class MySqlTest1 extends BaseMySqlTest{
+
+    @BeforeAll
+    static void init1(){
+        setDataDumpMySqlFile("emocije_data.sql");
+        setEmptyDumpMySqlFile("emocije_drop_dump.sql");
+
+        emptyData();
+        fillData();
+
+
+
+
+    }
+
+    @Test
+    void test() throws SQLException {
+        statement.executeUpdate("INSERT INTO emocije VALUES (NULL,'Radost',\"Nema puta ka sreći, sreća je put. – Buddha\")");
+
+
+    }
+
+    @AfterAll
+    static void showResult(){
+
+        showTable();
+        emptyData();
+
+    }
+
+
+}
---
+++ src/test/java/MySqlTest2.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class MySqlTest2 extends BaseMySqlTest{
+@Disabled
+    @BeforeAll
+    static void init1(){
+        setDataDumpMySqlFile("emocije_data.sql");
+        setEmptyDumpMySqlFile("emocije_drop_dump.sql");
+
+        emptyData();
+        fillData();
+
+
+
+
+    }
+
+    @Test
+    void test() throws SQLException {
+        statement.executeUpdate("INSERT INTO emocije VALUES (NULL,'Radost',\"Nema puta ka sreći, sreća je put. – Buddha\")");
+
+
+    }
+
+    @AfterAll
+    static void showResult(){
+
+        showTable();
+        emptyData();
+
+    }
+
+
+}
