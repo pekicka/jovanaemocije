0258dc92b736bab41578b9faf2e26acd8e6e74db
David Koloski <10221davidkoloski@etsntesla.edu.rs>
2024-03-05 13:29:56 +0100

    sesti video

---
+++ src/main/java/org/etsntesla/it/spring/BeanFactory.java
@@ -0,0 +1,24 @@
+package org.etsntesla.it.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@ComponentScan(basePackages = "org.etsntesla.it.spring")
+@PropertySource("classpath:application.properties")
+
+public class BeanFactory {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DatabaseConfiguration factoryDBConfig(){
+        DatabaseConfiguration config = new DatabaseConfiguration();
+        config.setUrl(env.getProperty("database.url"));
+        config.setUser(env.getProperty("database.user"));
+        config.setPass(env.getProperty("database.pass"));
+
+        return config;
+    }
+}
--- src/test/java/MySqlTest3.java
+++ src/test/java/MySqlTest3.java
@@ -1,14 +1,21 @@
+import org.etsntesla.it.spring.MySqlManagerBean;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.etsntesla.it.spring.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
-public class MySqlTest2 extends BaseMySqlTest{
+public class MySqlTest3 extends BaseMySqlTest{
 
+    static ApplicationContext ctx;
     @BeforeAll
-    static void init1(){
+    static void init1() throws SQLException{
+        ctx = new AnnotationConfigApplicationContext(BeanFactory.class);
+        statement = ctx.getBean(MySqlManagerBean.class).getConnection().createStatement();
         setDataDumpMySqlFile("emocije_data.sql");
         setEmptyDumpMySqlFile("emocije_drop_dump.sql");
 
